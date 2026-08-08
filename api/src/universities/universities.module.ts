2fa4f60ebaee1c0fa737e6af5484bfa1640ad1b1
Ayen Geoffrey Alexander <192260428+ayen01-crypto@users.noreply.github.com>
2026-08-08 15:47:29 +0300

    feat(api): compose universities module

---
+++ api/src/universities/universities.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { UniversitiesController } from './universities.controller.js';
+import { UniversitiesService } from './universities.service.js';
+
+@Module({
+  controllers: [UniversitiesController],
+  providers: [UniversitiesService],
+})
+export class UniversitiesModule {}
